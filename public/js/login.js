eb1c382de3eb85485393ff9faf7be23a0427ac88
“Jessica <wonjessica5@gmail.com>
2021-06-26 09:15:10 -0400

    updatesiter

--- public/js/login.js
+++ public/js/login.js
@@ -26,31 +26,3 @@ function validate(){
   }
 
 }
-
-function validate2(){
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  if ( email == "jane.doe@gmail.com" && password == "janedoe"){
-    window.location = "dashboard2.html"; // Redirecting to other page.
-    return false;
-  }
-  else {
-    //Makes red boxes around the form, outputs red text saying incorrect password, and shakes form
-    let div = document.querySelector('#email');
-    div.classList.remove('forminput');
-    div.classList.add('forminputred');
-    let div2 = document.querySelector('#password');
-    div2.classList.remove('forminput');
-    div2.classList.add('forminputred');
-    let div4 = document.querySelector('#incorrect');
-    div4.classList.remove('invis');
-    div4.classList.add('incorrect');
-    let div3 = document.querySelector('#formbox');
-    div3.classList.add('shake');
-    div3.addEventListener('animationend', function() {
-    		div3.classList.remove('shake');
-    });
-    return false;
-  }
-
-}
--- views/Fairshare Backup Green/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('.example-popover').popover({
-    container: 'body'
-  })
-})
-
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
